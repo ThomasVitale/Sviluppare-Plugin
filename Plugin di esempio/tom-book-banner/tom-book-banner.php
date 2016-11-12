78dec0426b41e05f112cb8c4f0c16c4682bd3d42
Thomas Vitale <thomasvitale93@gmail.com>
2016-11-12 08:36:32 +0100

    example plugin

---
+++ Plugin di esempio/tom-book-banner/tom-book-banner.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name: Tom Book Banner
+ * Plugin URI: http://wptorino.it/
+ * Description: Show a fixed banner to promote your brand new e-book.
+ * Version: 1.0
+ * Author: Thomas Vitale
+ * Author URI: https://thomasvitale.com
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wptorino
+ * Domain Path: /languages
+ *
+ * @package TomBookBanner
+ */
+
+/**
+ * Print an html banner with the promotional message.
+ *
+ * @since 1.0
+ */
+function tom_book_banner_html() {
+?>
+	 <div class="book-banner">
+	 	<p>
+			Sei appassionato di libri?
+			<a href="#">Scarica</a> gratuitamente il mio nuovo e-book!
+		</p>
+	 </div>
+<?php }
+
+add_action( 'wp_footer', 'tom_book_banner_html', 1 );
+
+/**
+ * Load the css to style the html banner.
+ *
+ * @since 1.0
+ */
+function tom_load_css() {
+	wp_enqueue_style( 'tom-book-banner', plugins_url( 'css/style.css', __FILE__ ) );
+}
+
+add_action( 'wp_enqueue_scripts', 'tom_load_css' );
